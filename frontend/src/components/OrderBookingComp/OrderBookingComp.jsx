5fedfa6a9d9f8b0b8758a143c4661ab4b6b84dd0
Lathika Sunder <lathikasunder11@gmail.com>
2024-05-16 22:00:31 +0530

    ADD - Post Booking to database

--- frontend/src/components/OrderBookingComp/OrderBookingComp.jsx
+++ frontend/src/components/OrderBookingComp/OrderBookingComp.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import "./OrderBookingComp.css";
 import carRepair from "../../assets/images/car-repair.png";
 import { FaLocationCrosshairs } from "react-icons/fa6";
@@ -11,19 +11,46 @@ import {
 import carCheck from "../../assets/images/car-check.png";
 import carWash from "../../assets/images/car-wash.png";
 import logo from "../../assets/images/shine-logo.png";
-import { SignedIn, SignedOut ,useUser} from "@clerk/clerk-react";
+import { SignedIn, SignedOut, useUser } from "@clerk/clerk-react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+ 
 
 const OrderBookingComp = () => {
-  const user=useUser()
-  const [service,setService]=useState('');
-  const [timeSlot,setTimeSlot]=useState('');
-  const [location,setLocation]=useState('')
+  const user = useUser();
+  const [service, setService] = useState("");
+  const [timeSlot, setTimeSlot] = useState("");
+  const [location, setLocation] = useState("");
 
-  const handleBooking=()=>{
-    const userId=user.id
-    const bookingDetails={userId,service,timeSlot,location};
-  }
+  const handleServiceChange = (selectedService) => {
+    setService(selectedService);
+    console.log("Service selected:", service);
+    
+  };
+
+  const handleSlotChange = (selectedSlot) => {
+    setTimeSlot(selectedSlot);
+    console.log("Time slot selected:", timeSlot);
+  };
+
+  const handleLocationChange = (selectedLocation) => {
+    setLocation(selectedLocation);
+    console.log("Selected Location: ",location)
+  };
+  const handleBooking = () => {
+    const userId = user.user.id;
+    const bookingDetails = { userId, service, timeSlot, location };
+    console.log(bookingDetails)
+
+    axios.post('http://localhost:4040/api/v1/shine/postBooking',bookingDetails)
+    .then(()=>{
+      console.log("Booking Success")
+    })
+    .catch((error)=>{
+      console.log("Client side error posting data",error)
+    })
+
+  };
   return (
     <div className="order-booking-comp">
       <div className="grid-columns">
@@ -33,7 +60,11 @@ const OrderBookingComp = () => {
               <div>
                 <MdLocalCarWash color="white" className="service-icon" />
               </div>
-              <div className="service-content" >
+
+              <div
+                className="service-content"
+                onClick={() => handleServiceChange("car-wash")}
+              >
                 <h2>Car Wash</h2>
                 <p>Restore Your Ride's Brilliance with Our Expert Wash.</p>
               </div>
@@ -42,19 +73,26 @@ const OrderBookingComp = () => {
               <div>
                 <MdCarRepair color="white" className="service-icon" />
               </div>
-              <div className="service-content" >
+
+              <div
+                className="service-content"
+                onClick={() => handleServiceChange("car-repair")}
+              >
                 <h2>Car Repair</h2>
                 <p>Keep Your Vehicle Running Smoothly with Our Care</p>
               </div>
             </div>
-            <div className="service-item" >
+            <div className="service-item">
               <div>
                 <MdOutlineMiscellaneousServices
                   color="white"
                   className="service-icon"
                 />
               </div>
-              <div className="service-content">
+              <div
+                className="service-content"
+                onClick={() => handleServiceChange("car-service")}
+              >
                 <h2>Car Service</h2>
                 <p>Trust Us - Revive Your Vehicle's Performance.</p>
               </div>
@@ -63,32 +101,32 @@ const OrderBookingComp = () => {
         </div>
         <div className="column">
           <div className="time-slots">
-            <div className="slot-container">
+            <div className="slot-container" onClick={()=>handleSlotChange('8-10')}>
               <p>
                 <span>8</span>am - <span>10</span>am
               </p>
             </div>
-            <div className="slot-container">
+            <div className="slot-container" onClick={()=>handleSlotChange('10-12')}>
               <p>
                 <span>10</span>am - <span>12</span>am
               </p>
             </div>
-            <div className="slot-container">
+            <div className="slot-container" onClick={()=>handleSlotChange('12-2')}>
               <p>
                 <span>12</span>pm - <span>2</span>pm
               </p>
             </div>
-            <div className="slot-container">
+            <div className="slot-container" onClick={()=>handleSlotChange('2-4')}>
               <p>
                 <span>2</span>pm - <span>4</span>am
               </p>
             </div>
-            <div className="slot-container">
+            <div className="slot-container" onClick={()=>handleSlotChange('4-6')}>
               <p>
                 <span>4</span>pm - <span>6</span>pm
               </p>
             </div>
-            <div className="slot-container">
+            <div className="slot-container" onClick={()=>handleSlotChange('6-8')}>
               <p>
                 <span>6</span>pm - <span>8</span>pm
               </p>
@@ -96,18 +134,18 @@ const OrderBookingComp = () => {
           </div>
 
           <div className="locations">
-            <div className="location">
+            <div className="location" onClick={()=>handleLocationChange('Chennai')}>
               <h2>
                 <FaLocationCrosshairs /> Chennai
               </h2>
             </div>
 
-            <div className="location">
+            <div className="location" onClick={()=>handleLocationChange('Mumbai')}>
               <h2>
                 <FaLocationCrosshairs /> Mumbai
               </h2>
             </div>
-            <div className="location">
+            <div className="location" onClick={()=>handleLocationChange('Hyderabad')}>
               <h2>
                 <FaLocationCrosshairs height={95} />
                 Hyderabad
@@ -115,14 +153,14 @@ const OrderBookingComp = () => {
             </div>
           </div>
           <SignedIn>
-            <Link to="/dashboard"  onClick={handleBooking}>
+            <Link to="/dashboard" onClick={handleBooking}>
               <button>
                 Book Now <MdNavigateNext color="next-icon" />
               </button>
             </Link>
           </SignedIn>
           <SignedOut>
-          <Link to="https://quick-moth-67.accounts.dev/sign-in?redirect_url=http%3A%2F%2Flocalhost%3A5173%2F">
+            <Link to="https://quick-moth-67.accounts.dev/sign-in?redirect_url=http%3A%2F%2Flocalhost%3A5173%2F">
               <button>
                 Book Now <MdNavigateNext color="next-icon" />
               </button>
